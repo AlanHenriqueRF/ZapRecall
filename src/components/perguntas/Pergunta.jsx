2f2d95d782960d98a1f26aef8ee4708c4015db2a
AlanHenriqueRF <alhenrangelfera@gmail.com>
2023-05-22 15:17:18 -0300

    Versão flashcards funcionando

--- src/components/perguntas/Pergunta.jsx
+++ src/components/perguntas/Pergunta.jsx
@@ -1,40 +1,170 @@
+import { useState } from 'react'
 import seta from '../../assets/seta_play.png'
 import styled from 'styled-components'
+import virar from '../../assets/seta_virar.png'
+import incorreto from '../../assets/icone_erro.png'
+import parcialmente from '../../assets/icone_quase.png'
+import correto from '../../assets/icone_certo.png'
 
-export default function Pergunta({pergunta,index}) {
-    function aparecepergunta(){
-        alert(`oi ${index}`)
+let tamanho = '20px'
+let color = '#333333'
+let decoracao = 'normal'
+let direcao = 'normal'
+let espaco = 'space-between'
+let margem = '0px'
+
+export default function Pergunta({ pergunta, index }) {
+    const imagens = [incorreto, parcialmente, correto]
+    const [indice, setIndice] = useState(-1)
+    const [aparece, setAparece] = useState([true, false]);
+    const [foto, setFoto] = useState(seta)
+
+
+
+    function novo_array() {
+        const novo_aparece = [...aparece]
+        console.log(novo_aparece)
+        if (novo_aparece[0] === true) {
+            return [!(novo_aparece[0]), novo_aparece[1]]
+        }
+        else if (novo_aparece[1] === false) {
+            return [(novo_aparece[0]), !(novo_aparece[1])]
+        }
+    }
+
+    function aparecepergunta(imagem) {
+        if (imagem === seta || imagem === virar) {
+            if (aparece[0] === false && aparece[1] === false) {
+                direcao = 'column'
+                espaco = 'space-evenly'
+            } else {
+                direcao = 'normal'
+                espaco = 'space-between'
+            }
+            if (aparece[0] === true && aparece[1] === false) {
+                margem = '18px'
+            }
+
+            console.log(margem)
+            color = '#333333'
+            decoracao = 'normal'
+            const novo_aparece = novo_array()
+            const novo_foto = novo_aparece[0] ? seta : (novo_aparece[1] ? false : virar);
+            tamanho = '23px';
+            setFoto(novo_foto)
+            setAparece(novo_aparece)
+        }
+
+    }
+
+    function verSeErrou() {
+        const novo_indice = 0;
+        const novo_aparece = [true, false]
+        direcao = 'normal'
+        espaco = 'space-between'
+        const nova_foto = imagens[novo_indice]
+        color = '#FF3030';
+        decoracao = 'line-through'
+        setFoto(nova_foto)
+        setAparece(novo_aparece)
+        setIndice(novo_indice)
+
+    }
+
+    function verSeQuase() {
+        const novo_indice = 1;
+        direcao = 'normal'
+        espaco = 'space-between'
+        const novo_aparece = [true, false]
+        const nova_foto = imagens[novo_indice]
+        color = '#FF922E';
+        decoracao = 'line-through'
+        setFoto(nova_foto)
+        setAparece(novo_aparece)
+        setIndice(novo_indice)
+
+    }
+
+    function verSeAcertou() {
+        const novo_indice = 2;
+        direcao = 'normal'
+        espaco = 'space-between'
+        const novo_aparece = [true, false]
+        const nova_foto = imagens[novo_indice]
+        color = '#2FBE34';
+        decoracao = 'line-through'
+        setFoto(nova_foto)
+        setAparece(novo_aparece)
+        setIndice(novo_indice)
+
     }
     return (
-        <Question>
-            <h2>Pergunta {index+1}</h2>
-            <img src={seta} onClick={()=>aparecepergunta()} alt="seta" />
+        <Question aparece={aparece} tamanho={tamanho} color={color} decoracao={decoracao} direcao={direcao} espaco={espaco} margem={margem}>
+
+            <h2>{aparece[0] ? `Pergunta ${index + 1}` : (aparece[1] ? pergunta.answer : pergunta.question)}</h2>
+
+            {foto ? <img src={indice === -1 ? foto : imagens[indice]} onClick={() => aparecepergunta(foto)} alt="seta" /> : <Botoes><button onClick={verSeErrou}>Não lembrei</button> <button onClick={verSeQuase}>Quase não lembrei</button> <button onClick={verSeAcertou}>Zap!</button></Botoes>}
+
         </Question>
     )
 }
 
 const Question = styled.li`
-    background-color:#FFFFFF;
+    background-color:${props => props.aparece[0] ? '#FFFFFF' : '#FFFFD4'};
     border-radius:5px;
     width:300px;
-    height:65px;
+    height:${props => props.aparece[0] ? '65px' : '131px'};
     display: flex;
-    justify-content: space-between;
-    align-items:center;
-    margin: 0px 0px 25px 0px ;
+    flex-direction: ${props => props.direcao};
+    justify-content: ${props => props.espaco};
+    align-content: space-between;
+    align-items:${props => props.aparece[0] ? 'center' : ''};
+    margin: 0px 0px 25px 0px;
     box-shadow: 0 4px 5px 0px rgba(0, 0, 0, 0.15);
     h2{
         font-family: 'Recursive', sans-serif;
-        font-size:16px;
-        font-weight:700;
-        color: #333333;
+        font-size: ${props => props.aparece[0] ? '16px' : '18px'};
+        font-weight: ${props => props.aparece[0] ? '700' : '400'};
+        color: ${props => props.color};
+        text-decoration: ${props => props.decoracao}; 
         margin-left:15px;
+        margin-top:${props => props.aparece[0] ? '0px' : '15px'};
     }
 
     img{
         color:#333333;
-        width:20px;
-        height:23px;
-        margin-right:22px;
+        width: ${props => props.aparece[0] ? props.tamanho : '30px'};
+        height: ${props => props.aparece[0] ? '23px' : '20px'};
+        margin-right: 22px;
+        margin-top:${props => props.aparece[0] ? '0px' : '105px'};
+    }
+    
+`
+
+const Botoes = styled.div`
+    display:flex;
+    align-items:center;
+    justify-content:space-evenly;
+    button{
+        border-radius:5px;
+        width:86px;
+        height:38px;
+        font-family: 'Recursive', sans-serif;
+        font-weight:400;
+        font-size:12px;
+        color:#FFFFFF;
+        border:none;
+    }
+
+    & :nth-child(1){
+        background-color:#FF3030;
+    }
+
+    & :nth-child(2){
+        background-color:#FF922E;
+    }
+
+    & :nth-child(3){
+        background-color:#2FBE34;
     }
 `
